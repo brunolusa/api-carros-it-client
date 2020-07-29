663c14b00d6c32600222de23df05fb508832c02c
Bruno Lusa <bruno.lusa@gmail.com>
2020-07-29 19:38:33 -0300

    Criando método alterarCarros no PUT client

--- src/main/java/com/lusa/carros/clients/PutCarrosClient.java
+++ src/main/java/com/lusa/carros/clients/PutCarrosClient.java
@@ -1,4 +1,28 @@
 package com.lusa.carros.clients;
 
-public class PutCarrosClient {
+import com.lusa.carros.model.Carro;
+import com.lusa.carros.utils.BaseApi;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.apache.http.HttpStatus;
+
+import static com.lusa.carros.utils.StaticValues.*;
+import static io.restassured.RestAssured.given;
+
+public class PutCarrosClient extends BaseApi {
+
+    public PutCarrosClient() { configBaseApi(); }
+
+    public ValidatableResponse alterarCarros(Carro c, Long id){
+        return
+            given().
+                spec(spec).
+                pathParam(PARAM_PATH_ID, id).
+                body(c).
+            when().
+                put(CARROS_WITH_ID).
+            then().
+                statusCode(HttpStatus.SC_OK).
+                contentType(ContentType.JSON);
+    }
 }
