5579311c1ab1fbefaa4a83285ebd9625ece768a8
Bruno Lusa <bruno.lusa@gmail.com>
2020-07-29 19:15:28 -0300

    Inserindo getCarrosClient

--- src/main/java/com/lusa/carros/clients/GetCarrosClient.java
+++ src/main/java/com/lusa/carros/clients/GetCarrosClient.java
@@ -1,4 +1,38 @@
 package com.lusa.carros.clients;
 
-public class GetCarrosClient {
+import com.lusa.carros.utils.BaseApi;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.apache.http.HttpStatus;
+
+import static com.lusa.carros.utils.StaticValues.*;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+public class GetCarrosClient extends BaseApi {
+
+    public GetCarrosClient() { configBaseApi(); }
+
+    public ValidatableResponse getAllCarros(){
+       return
+           given().
+               spec(spec).
+           when().
+               get(CARROS).
+           then().
+               statusCode(HttpStatus.SC_OK).
+               contentType(ContentType.JSON);
+    }
+
+    public ValidatableResponse getCarroById(Long id){
+        return
+            given().
+                spec(spec).
+                pathParam(PARAM_PATH_ID, id).
+            when().
+                get(CARROS_WITH_ID).
+            then().
+                statusCode(HttpStatus.SC_OK).
+                contentType(ContentType.JSON);
+    }
 }
--- src/main/java/com/lusa/carros/utils/StaticValues.java
+++ src/main/java/com/lusa/carros/utils/StaticValues.java
@@ -4,4 +4,5 @@ public class StaticValues {
     public static final String PARAM_PATH_ID = "id";
     public static final String CARROS_WITH_ID = "/carros/{id}";
     public static final String ACTUATOR = "/actuator/health";
+    public static final String CARROS = "/carros";
 }
