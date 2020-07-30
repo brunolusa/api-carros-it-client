ed3b86398bbade55ff4de63c7c1afa2e944ec5be
Bruno Lusa <bruno.lusa@gmail.com>
2020-07-29 22:14:17 -0300

    Removendo dependencias nao utilizadas

--- src/main/java/com/lusa/carros/utils/PropertyLoader.java
+++ src/main/java/com/lusa/carros/utils/PropertyLoader.java
@@ -1,16 +1,14 @@
 package com.lusa.carros.utils;
 
 import com.lusa.carros.customexceptions.NoPropertiesException;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.MessageFormat;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 public class PropertyLoader {
-    private static final Logger LOGGER = LogManager.getLogger();
-
+    protected static Logger logger;
     private PropertyLoader() {}
 
     public static String retornarValorArquivoConfiguracao(final String propriedade) {
@@ -20,11 +18,11 @@ public class PropertyLoader {
                 .getResourceAsStream("conf/config.properties")) {
 
             properties.load(propFileInpStream);
-            LOGGER.info(MessageFormat.format("Localizado a propriedade: {0} no arquivo conf/config.properties com o valor {1}", propriedade, properties.getProperty(propriedade)));
+            logger.info(MessageFormat.format("Localizado a propriedade: {0} no arquivo conf/config.properties com o valor {1}", propriedade, properties.getProperty(propriedade)));
 
             return properties.getProperty(propriedade);
         } catch (IOException e) {
-            LOGGER.error(MessageFormat.format("Propriedade {0} não foi encontrada nos arquivos de configuração", propriedade), e);
+            logger.warning(MessageFormat.format("Propriedade {0} não foi encontrada nos arquivos de configuração", propriedade));
             throw new NoPropertiesException("Sem arquivo de configuração!");
         }
     }
